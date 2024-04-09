d3a3145ca6e5bc1cde802bdcfe3d036635b71ed1
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-04-09 23:46:43 +0900

    型を指定

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,24 @@
 <?php
-echo 'Welcome TECH QUEST!';
+class WordChecker {
+  // $wordに整数型を指定 || 返り値として void を指定
+  public static function printWordDescription(string $word): void {
+    for ($i = 1; $i <= 6; $i++) {
+      if ($word == "hoge") {
+          if ($i % 2 == 0) {
+              echo "wordがhoge かつ " . $i . "は2の倍数です" . "\n";
+          } else {
+              echo "wordがhoge かつ " . $i . "は2の倍数以外です" . "\n";
+          }
+      } else {
+          if ($i % 2 == 0) {
+              echo "wordがhogeでない かつ " . $i . "は2の倍数です" . "\n";
+          } else {
+              echo "wordがhogeでない かつ " . $i . "は2の倍数以外です" . "\n";
+          }
+      }
+    }
+  }
+}
+
+WordChecker::printWordDescription("hoge");
+?>
